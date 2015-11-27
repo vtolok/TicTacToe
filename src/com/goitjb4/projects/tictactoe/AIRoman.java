227db23ea9a7fcf949f26118c146c369f3a20cbe
nikolifan <nikolifan15@gmail.com>
2015-11-27 17:04:40 +0200

    fixed AIRoman

--- src/com/goitjb4/projects/tictactoe/AIRoman.java
+++ src/com/goitjb4/projects/tictactoe/AIRoman.java
@@ -5,23 +5,18 @@ import java.util.List;
 
 public class AIRoman extends AbstractAI {
 
+	private static final int PLAYER_X = 1;
+	private static final int PLAYER_O = -1;
 	
-	public static final int UNCLEAR_SITUATION = 2;
-	public static final int DRAW = 3;
-	public static final int I_CAN_WIN = 4;
-	public static final int ENEMY_CAN_WIN = 5;
+	private static final int RATIO = 100; 
+	private static final int X_WIN = PLAYER_X * RATIO;
+	private static final int O_WIN = PLAYER_O * RATIO;
+	private static final int DRAW = 0;
 
-	public static final int CENTER_OF_THE_BOARD = 4;
-
-	public static final int CORNER_1 = 0;
-	public static final int CORNER_2 = 2;
-	public static final int CORNER_3 = 6;
-	public static final int CORNER_4 = 8;
-
-	static List<Abc> listWinLines = new ArrayList<Abc>();
+	private List<Abc> listWinLines = new ArrayList<Abc>();
+	private int[] inputBoard;
 
 	public AIRoman() {
-
 		listWinLines.add(new Abc(0, 1, 2));
 		listWinLines.add(new Abc(3, 4, 5));
 		listWinLines.add(new Abc(6, 7, 8));
@@ -35,191 +30,87 @@ public class AIRoman extends AbstractAI {
 
 	}
 
-	public static boolean checkLine(int x, int[] board, int a, int b, int c) {
-		return (board[a] == x) & (board[b] == x) & (board[c] == x);
-	}
-
-	private static boolean playerCanWin(int player, int[] board) {
-		boolean res = false;
-
+	private boolean playerWin(int player) {
 		for (Abc e : listWinLines) {
-			res = checkLine(player, board, e.getA(), e.getB(), e.getC());
-			if (res == true) {
+			if ((inputBoard[e.getA()] == player) && (inputBoard[e.getB()] == player)
+					&& (inputBoard[e.getC()] == player)) {
 				return true;
 			}
 		}
-
-		return res;
-
+		return false;
 	}
 
 	@Override
-	public int move( int[] board, int player) {
-
-		int[] inputBoard = board.clone();
-		int countFillCells = countOfFillCells(board);
-
-		if (countFillCells <= 1) {
-			return getIndexOfGoodPosition(board);
+	 public int move(int[] board, int player) {
+		inputBoard = board.clone();
+		int maxIndex = -1;
+		if (boardIsEmpty(inputBoard)) {
+			maxIndex = (int) (Math.random() * (inputBoard.length));
 		} else {
-			return benefitsOfProgress(player, inputBoard, null);
+			int res = benefitsOfProgress(player, inputBoard, 0);
+			maxIndex = res - cutOffMove(res);
 		}
-
-	}
-
-	private static int benefitsOfProgress(int player, int[] board, Integer cell) {
-
-		int maxValue, maxIndex, resValue;
-		resValue = maxValue = 0;
-		maxIndex = -1;
-		int enemy = -player;
-
-		int[] localBoard = board.clone();
-		if (cell != null) {
-			localBoard[cell] = enemy;
-
-			return getWeigthOfThatStep(enemy, localBoard);
-
-		} else {
-
-			for (int i = 0; i < localBoard.length; i++) {
-
-				localBoard = board.clone();
-				if (localBoard[i] == 0) {
-					localBoard[i] = player;
-					if (playerCanWin(player, localBoard)) {
-						return i;// WIN;
-					}
-
-					resValue = benefitsOfProgress(player, localBoard, i);
-
-					if (resValue >= maxValue) {
-						maxValue = resValue;
-						maxIndex = i;
-					}
-
-				}
-			}
-		}
-
-		if (maxValue == UNCLEAR_SITUATION) {
-			int IndexOfGoodPos = getIndexOfGoodPosition(localBoard);
-			if (IndexOfGoodPos != -1) {
-				maxIndex = IndexOfGoodPos;
-			}
-		}
-
 		return maxIndex;
 	}
 
-	public static int getWeigthOfThatStep(int enemy, int[] board) {
-		/*if (playerCanWin(enemy, board)) {
-			return ENEMY_CAN_WIN;
+	
+	private int cutOffMove(int i) {
+		if (i == 0) {
+			return i;
+		}
+		return ((int) (i / RATIO)) * RATIO;
+	}
+	
+	private int benefitsOfProgress(int player, int[] board, int depth) {
+		if (playerWin(PLAYER_X)) {
+			return X_WIN;
+		} else if (playerWin(PLAYER_O)) {
+			return O_WIN;
+		} else if (!canMove(board)) {
+			return DRAW;
 		}
 
-		if (canMove(board))
-			return UNCLEAR_SITUATION;
-		else
-			return DRAW;*/
-
-		int minValue = UNCLEAR_SITUATION;
-		int player = enemy;
-		int resValue = UNCLEAR_SITUATION;
-		
-		int[] localBoard = board.clone();
-		
-		for (int i = 0; i < localBoard.length; i++) {
-
-			localBoard = board.clone();
-			if (localBoard[i] == 0) {
-				localBoard[i] = player;
-				if (playerCanWin(player, localBoard)) {
-					return ENEMY_CAN_WIN;// WIN;
+		int resVal = 0;
+		if (player == PLAYER_X) {
+			resVal = O_WIN;
+		} else {
+			resVal = X_WIN;
+		}
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == 0) {
+				board[i] = player;
+				int enemyVal = benefitsOfProgress(-player, board, depth + 1);
+				//System.out.println("player = " + player + ", move = " + i + ", resVal = " + resVal + ", enemyVal = " + enemyVal + ", depth = " + depth);
+				if (((player == PLAYER_X) && (cutOffMove(enemyVal) > cutOffMove(resVal)))
+						|| ((player == PLAYER_O) && (cutOffMove(enemyVal) < cutOffMove(resVal)))) {
+					resVal = cutOffMove(enemyVal);
+					if (resVal < 0) {
+						resVal -= i; 
+					} else {
+						resVal += i;
+					}
 				}
-				
-
-				resValue = getWeigthOfThatStep(player, localBoard);
-
-				if (resValue <= minValue) {
-					minValue = resValue;
-				}
-
+				board[i] = 0;
 			}
 		}
-		return minValue;
-		
+		return resVal;
 	}
 
-	public static int countOfFillCells(int[] board) {
-		int count = 0;
-		for (int intCell : board) {
-			if (intCell != 0) {
-				count++;
-			}
+	private boolean boardIsEmpty(int[] board) {
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] != 0)
+				return false;
 		}
-		return count;
-	}
-
-	public static boolean canMove(int[] board) {
+		return true;
+	}	
+	
+	private boolean canMove(int[] board) {
 
 		for (int i = 0; i < board.length; i++) {
 			if (board[i] == 0)
 				return true;
 		}
 		return false;
-	}
-
-	public static int getIndexOfGoodPosition(int[] board) {
-		int res = -1;
-		if (board[CENTER_OF_THE_BOARD] == 0) {
-			return CENTER_OF_THE_BOARD;
-		} else if (board[CORNER_1] == 0) {
-			return CORNER_1;
-		} else if (board[CORNER_2] == 0) {
-			return CORNER_2;
-		} else if (board[CORNER_3] == 0) {
-			return CORNER_3;
-		} else if (board[CORNER_4] == 0) {
-			return CORNER_4;
-		}
-		return res;
-	}
-
-	public static int move(int[] board, int player, AbstractAI t) {
-		int[] param = board.clone();
-		return t.move( param, player);
-
-	}
-
-	public static void NextMove(int player, int[] board, AbstractAI ticTacImpls) {
-		int[] param = board.clone();
-
-		int resIndex;
-
-		resIndex = ticTacImpls.move(param, player);
-		if ((resIndex >= 0) && (board[resIndex] == 0)) {
-			board[resIndex] = player;
-			ShowTheBoard(board);
-		}
-
-	}
-
-	public static void ShowTheBoard(int[] board) {
-		int counter = 0;
-		System.out.println("-------");
-		for (int i : board) {
-			counter++;
-
-			if (i >= 0) {
-				System.out.print(" " + i + " ");
-			} else
-				System.out.print(i + " ");
-
-			if (counter % 3 == 0) {
-				System.out.println("");
-			}
-
-		}
 	}
 
 	private class Abc {
